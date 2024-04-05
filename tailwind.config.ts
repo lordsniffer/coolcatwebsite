eb609fa36374018b6a361b1ae0d385763821b9d9
lordsniffer <166129919+lordsniffer@users.noreply.github.com>
2024-04-05 14:03:54 +0200

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "blue": "#2FAAFE",
+        "bg": "#222831",
+        "bg-light": "#393E46",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
